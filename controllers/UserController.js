57f950b5c745a85cb73073edd746b6e6c7d460cd
Gabriel-biel <gabriel97gla98@gmail.com>
2022-01-06 16:38:52 -0300

    fix bugs: bugs na view

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,7 +3,7 @@ class UserController {
     this.fomrEl = document.getElementById(formId);
     this.tableEl = document.getElementById(tableId);
     
-    this.onSubmit();
+    this.onSubmit(); 
   }
 
   onSubmit(){
@@ -11,17 +11,20 @@ class UserController {
       event.preventDefault();
 
       let values = this.getValues()
-      
-      this.getPhoto((content) => {
-        values.photo = content;
-        this.addLine(values);
-      });
-
+      this.getPhoto().then(
+        (content) => {
+          values.photo = content;
+          this.addLine(values);
+        }, 
+        (e) => {
+          console.error(e);
+      })
     });
   }
 
-  getPhoto(callback){
-    let fileReader = new FileReader();
+  getPhoto(){
+    return new Promise((resolve, reject) => {
+      let fileReader = new FileReader();
 
     let elements = [...this.fomrEl.elements].filter(item => {
       if (item.name === 'photo'){
@@ -32,10 +35,19 @@ class UserController {
     let file = elements[0].files[0]
 
     fileReader.onload = () => {
-      callback(fileReader.result); 
+      resolve(fileReader.result); 
     };
 
-    fileReader.readAsDataURL(file);
+    fileReader.onerror = (e) =>{
+      reject(e);
+    }
+
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }else {
+     resolve('dist/img/boxed-bg.jpg');
+    }
+    })
   }
 
   getValues(){
@@ -48,7 +60,10 @@ class UserController {
         if(field.checked){
           user[field.name] = field.value
         }
-      }else {
+      }else if(field.name == 'admin') {
+        user[field.name] = field.checked;
+      }
+      else {
         user[field.name] = field.value
       }
     });
@@ -65,19 +80,21 @@ class UserController {
   }
 
   addLine(dataUser) {
-    
-    this.tableEl.innerHTML = `
-      <tr>
-        <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-        <td>${dataUser.name}</td>
-        <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
-        <td>${dataUser.birth}</td>
-        <td>
-          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-      </tr>
+
+    let tr = document.createElement('tr');
+    tr.innerHTML =
     `
+      <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+      <td>${dataUser.name}</td>
+      <td>${dataUser.email}</td>
+      <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+      <td>${dataUser.birth}</td>
+      <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+      </td>
+    `
+    
+    this.tableEl.appendChild(tr); 
   }
 }
